d2cec2325fc29cba8868052eea3329a3c1bb5d06
Malvadoyael <yaeltapia@gmail.com>
2025-12-18 17:00:40 -0600

    feat: inicializacion backend FlightOnTime

---
+++ src/test/java/com/flightontime/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightontime.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
